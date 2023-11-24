984dd8d539492c04abc5e6bc6eb008ae43d8de06
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-11-24 03:51:47 +0300

    feat: create basic router and files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import styles from './App.module.css';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Projects from './pages/Projects/Projects';
 
 const App = () => {
   return (
-    <div className={styles.app}>App</div>
+    <div className={styles.app}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+
+      </BrowserRouter>
+
+    </div>
   )
 }
 
