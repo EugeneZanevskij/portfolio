2a3d8b519a5f40d24161d4142a651693fc0304ae
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-11-25 16:09:44 +0300

    feat: create first home section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import styles from './Home.module.css';
+import homeImage from '../../assets/homeImage.jpg';
 
-const Home = () => {
+const Home: React.FC = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className={styles.home}>
+      <div className={styles.homeContent}>
+        <h1 className={styles.homeTitle}>Hi, I'm Eugene</h1>
+        <p className={styles.homeInfo}>I'm a Front End developer with a passion for creating websites. Looking for work opportunities</p>
+        <a href="/" target="_blank" className={styles.homeCvBtn}>View CV</a>
+      </div>
+      <img src={homeImage} alt="Home" className={styles.homeImage} />
+      <div className={styles.topBlur} />
+    </section>
+  );
+};
 
-export default Home
+export default Home;
